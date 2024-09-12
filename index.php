27e6e6e90bb2f8d38e29fa3089201e8ad631aa07
briankihia <brianjohnkiama@gmail.com>
2024-09-12 09:28:35 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  echo"I really love pizza<br>";
+  echo"it's really good";
+  // this is a comment
+  /* this is a mutiline comment
+  */
+
+?>
+<!-- good thing about php is that you can have your html code within your php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <br>
+  <button>Order pizza</button>
+  
+</body>
+</html>
